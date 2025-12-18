cdfde1a51e7bae85a7eae1f884ae98ca7d67e3f0
bala-amypo <bala@amypo.in>
2025-12-18 09:51:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/Employee.java
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -1,2 +1,43 @@
-package com.example.demo.entity;
++package com.example.demo.Entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+
+public class Employee {
+    @Id
+    private Long id;
+    private String fullName;
+    @Column(unique=true)
+    private String email;
+    private String department;
+    private String jobTitle;
+    public Long getId() {
+        return id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public String getJobTitle() {
+        return jobTitle;
+    }
+    public boolean isActive() {
+        return active;
+    }
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+    public LocalDate getUpdatedAt() {
+        return updatedAt;
+    }
+    private boolean active;
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+}
