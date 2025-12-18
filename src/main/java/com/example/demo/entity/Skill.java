e2921933fbcfc9744e22f2f3b67c290749f7ee96
bala-amypo <bala@amypo.in>
2025-12-18 08:46:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/Skill.java
+++ src/main/java/com/example/demo/entity/Skill.java
@@ -1,8 +1,13 @@
 package com.example.demo.entity;
 
 public class Skill {
+    @Id
     private Long id;
     private String fullName;
-    @
-    private 
+    @Column(unique=true)
+    private String email;
+    private String department;
+    private String jobTitle;
+    private boolean active;
+    
 }
