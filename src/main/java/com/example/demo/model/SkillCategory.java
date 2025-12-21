d041b63debc59dfa7e11813d9a686a4af8f03cc0
bala-amypo <bala@amypo.in>
2025-12-21 18:19:46 +0000

    User commit

--- src/main/java/com/example/demo/model/SkillCategory.java
+++ src/main/java/com/example/demo/model/SkillCategory.java
@@ -1,37 +1,53 @@
-package com.example.demo.Entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
+package com.example.demo.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "skill_categories")
 public class SkillCategory {
+
     @Id
-    private long id;
-    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String categoryName;
+
     private String description;
-    private boolean active;
-    public long getId() {
-        return id;
+
+    private Boolean active = true;
+
+    public Long getId(){
+        return id; 
     }
-    public void setId(long id) {
+    public void setId(Long id){
         this.id = id;
     }
-    public String getCategoryName() {
-        return categoryName;
+    public String getCategoryName(){ 
+        return categoryName; 
     }
-    public void setCategoryName(String categoryName) {
+    public void setCategoryName(String categoryName){ 
         this.categoryName = categoryName;
     }
-    public String getDescription() {
+    public String getDescription(){
         return description;
     }
-    public void setDescription(String description) {
+    public void setDescription(String description){
         this.description = description;
     }
-    public boolean isActive() {
+    public Boolean getActive(){
         return active;
     }
-    public void setActive(boolean active) {
+    public void setActive(Boolean active){
+        this.active = active;
+    }
+    public SkillCategory() {}
+
+    public SkillCategory(Long id,String categoryName, String description, Boolean active) {
+        this.id=id;
+        this.categoryName = categoryName;
+        this.description = description;
         this.active = active;
     }
 }
